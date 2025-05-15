b4a641994971faa4f65a9d8565b90a4834162eef
Kotra Sai Sagar <116935024+saisagar253@users.noreply.github.com>
2025-05-15 15:42:45 +0530

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.sidebar:
 generic_url=st.text_input("URL",label_visibility="collapsed")
 
 ## Gemma Model USsing Groq API
-llm =ChatGroq(model="Gemma-7b-It", groq_api_key="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")
+llm =ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
 
 prompt_template="""
 Provide a summary of the following content in 300 words:
